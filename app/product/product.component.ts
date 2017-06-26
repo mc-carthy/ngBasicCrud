9eb9c8b4e65862bdb626e24619198c93288af48c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-26 01:33:45 +0100

    Get products from service and display them on the template

--- app/product/product.component.ts
+++ app/product/product.component.ts
@@ -1,10 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     moduleId: module.id,
     templateUrl: 'product.template.html'
 })
 
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+
+    products: Product[];
+
+    constructor (
+        private productService : ProductService
+    )
+    { }
+
+    ngOnInit()
+    {
+        this.getProducts();
+    }
+
+    getProducts()
+    {
+        this.products = this.productService.getProducts();
+    }
 
 }
--- app/product/product.service.ts
+++ app/product/product.service.ts
@@ -5,4 +5,27 @@ import { Product } from './product';
 export class ProductService {
 
     constructor() { }
+
+    getProducts(): Product[] {
+        return [
+            {
+                id: 1,
+                name: "Scissors",
+                description: "They cut things",
+                price: 4.99
+            },
+            {
+                id: 2,
+                name: "Steak knives",
+                description: "They cut other things",
+                price: 10.99
+            },
+            {
+                id: 3,
+                name: "Shot Glass",
+                description: "They hold shots",
+                price: 7.99
+            },
+        ]
+    }
 }
